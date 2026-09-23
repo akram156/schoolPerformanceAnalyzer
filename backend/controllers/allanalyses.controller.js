110cf8a80d20bd11a8ab266f8cb22bf1826a89e9
akram156 <akrambenchouche240@gmail.com>
2026-09-23 13:12:33 +0100

    still trying to fix

--- backend/controllers/allanalyses.controller.js
+++ backend/controllers/allanalyses.controller.js
@@ -3,12 +3,13 @@ const jwt = require("jsonwebtoken");
 exports.getAllAnalyses = async (req, res) => {
   try {
     const token = req.headers["authorization"];
+    console.log("token", token);
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-    const result = await Analyses.find({user:decodedToken.id}).select(
+    const result = await Analyses.find({ user: decodedToken.id }).select(
       "_id name educationalLevel schoolYear stream trimester result.overview createdAt",
     );
-    console.log("result",result)
-    console.log("decode token", decodedToken)
+    console.log("decode token", decodedToken);
+    console.log("result", result);
     return res.status(200).json({
       analyses: result,
     });
